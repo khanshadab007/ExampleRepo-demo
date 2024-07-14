eeba87873b7ecaf20791a3b52268d2af814fad56
khanshadab007 <khanshadabqasim@gmail.com>
2024-07-14 19:58:50 +0530

    Added python main file

---
+++ main.py
@@ -0,0 +1,11 @@
+def linear_search(input_list, element):
+    for index, value in enumerate(input_list):
+        if value == element:
+            return index
+
+    return -1
+
+
+uinput_list = [3, 4, 1, 6, 14]
+uelement = 4
+print("Index position for the element x is:", linear_search(uinput_list, uelement))
